c37f402b3962e488a93b0fa791e3d0eadb192b51
SMooreSwe <118370783+SMooreSwe@users.noreply.github.com>
2023-03-02 10:40:54 +0100

    Fixed fetch functionality and photo gallery rendering

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 
 type Photo = {
+    id: string,
     urls: { small:string },
     alt_description: string
   };
@@ -9,17 +10,11 @@ export const Main = () => {
     const [search, setSearch] = useState<string>('')
     const [data, setData] = useState<Photo[]>([])
     // const [count, setCount] = useState(1)
-    
-    // const apiCall = async (search: string, count: number) => {
-    //     const photos = await fetch(`https://api.unsplash.com/search/photos?page=${count}&query=${search}&client_id=${process.env.REACT_APP_ACCESS_KEY}`);
-    //     const data = await photos.json();
-    //     return data.results;
-    // };
 
     useEffect(() => {
         fetch(`https://api.unsplash.com/search/photos?page=1&query=${search}&client_id=${process.env.REACT_APP_ACCESS_KEY}`)
           .then(response => response.json())
-          .then(response => setData(response.result))
+          .then(response => setData(response.results))
     }, [search]);
 
     const searchBar = useRef<HTMLInputElement | null>(null)
@@ -32,21 +27,22 @@ export const Main = () => {
         <>
             <input ref={searchBar} type="text" />
             <button onClick={searchInput}>Search</button>
-            {data && console.log(search, data)/* {data && data.map((photo: Photo) => { return <img className="main-container__photo" src={photo.urls.small} alt={photo.alt_description} />})} */}
-        </>);
+            <div className="main-container__images" id="main-container__images">
+            {data && data.map((photo: Photo) => {return(  
+                <div key={photo.id} className="flip-card">
+                    <div className="flip-card-inner">
+                        <div className="flip-card-front">
+                            <img className="main-container__photo" src={photo.urls.small} alt={photo.alt_description} />
+                        </div>
+                        <div className="flip-card-back">
+                            <h1>Unsplash link</h1>
+                            <p>photographer name</p>
+                            <p>photographer profile link</p>
+                        </div>
+                    </div>
+                </div>
+            )})}
+            </div>
+        </>
+    );
 }
-
-// const result = data.result.map((photo: Photo) => { (
-//     <div className="flip-card">
-//       <div className="flip-card-inner">
-//         <div className="flip-card-front">
-//           <img className="main-container__photo" src={photo.urls.small} alt={photo.alt_description} />
-//         </div>
-//         <div className="flip-card-back">
-//           <h1>Unsplash link</h1>
-//           <p>photographer name</p>
-//           <p>photographer profile link</p>
-//         </div>
-//       </div>
-//     </div>
-//     )});
