8cffe1df8657f6df0e225728b557b9a6dfcc1b8d
SMooreSwe <118370783+SMooreSwe@users.noreply.github.com>
2023-03-02 09:27:09 +0100

    search bar and button added

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 type Photo = {
     urls: { small:string },
@@ -16,17 +16,24 @@ export const Main = () => {
     //     return data.results;
     // };
 
-    
-
     useEffect(() => {
-        fetch(`https://api.unsplash.com/search/photos?page=${count}&query=${search}&client_id=${process.env.REACT_APP_ACCESS_KEY}`)
+        fetch(`https://api.unsplash.com/search/photos?page=1&query=${search}&client_id=${process.env.REACT_APP_ACCESS_KEY}`)
           .then(response => response.json())
           .then(response => setData(response.result))
     }, [search, count]);
 
-    return (<main>{
-        data.map((photo: Photo) => <img className="main-container__photo" src={photo.urls.small} alt={photo.alt_description} />)
-                }</main>);
+    const searchBar = useRef<HTMLInputElement | null>(null)
+    const searchInput = () => {
+        console.log(searchBar.current!.value)
+        setSearch(searchBar.current!.value)
+    }
+    
+    return (
+        <main>
+            <input ref={searchBar} type="text" />
+            <button onClick={searchInput}>Search</button>
+            {data && data.map((photo: Photo) => { return <img className="main-container__photo" src={photo.urls.small} alt={photo.alt_description} />})
+        }</main>);
 }
 
 // const result = data.result.map((photo: Photo) => { (
